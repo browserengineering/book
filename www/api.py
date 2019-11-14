403a9f5a8fe4c6d43760c42edd09d8097b0a7216
Pavel Panchekha <me@pavpanchekha.com>
2019-11-13 22:04:04 -0700

    Developed new feedback / api server

---
+++ www/api.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python3
+
+import bottle
+import json
+import os
+import pickle
+import time
+import difflib
+import html
+
+bottle.TEMPLATE_PATH.append(".")
+
+class Data:
+    def __init__(self, filename):
+        self.filename = filename
+        if os.path.exists(filename):
+            with open(filename, "rb") as f:
+                self.data = pickle.load(f)
+        else:
+            self.data = []
+
+    def save(self):
+        with open(self.filename, "wb") as f:
+            pickle.dump(self.data, f)
+
+    def typo(self, url, old, new):
+        self.data.append({
+            'id': len(self.data),
+            'time': time.time(),
+            'type': 'typo',
+            'url': url,
+            'old': old,
+            'new': new,
+            'status': 'new'
+        })
+        self.save()
+
+    def comment(self, url, text, comment):
+        self.data.append({
+            'id': len(self.data),
+            'time': time.time(),
+            'type': 'comment',
+            'url': url,
+            'text': text,
+            'comment': comment,
+            'status': 'new'
+        })
+        self.save()
+
+    def status(self, i):
+        return self.data[i]["status"]
+
+    def set_status(self, i, status):
+        self.data[i]['status'] = status
+        self.save()
+
+    def __iter__(self):
+        return iter(self.data)
+
+DATA = Data("db.pickle")
+
+@bottle.route("/api/typo", method=["POST", "OPTIONS"])
+def typo():
+    data = json.load(bottle.request.body)
+    DATA.typo(data["url"], data["old"], data["new"])
+
+@bottle.route("/api/comment", method=["POST", "OPTIONS"])
+def comment():
+    data = json.load(bottle.request.body)
+    DATA.comment(data["url"], data["text"], data["comment"])
+
+def prettify(obj):
+    if obj['type'] != 'typo': return obj
+    old, new = obj['old'], obj['new']
+    old_words = [w + "\n" for w in old.split()]
+    new_words = [w + "\n" for w in new.split()]
+
+    d = difflib.Differ()
+    results = []
+    state = " "
+    for out in d.compare(old_words, new_words):
+        new_state = out[0]
+        word = out[2:-1]
+        if new_state == "?": continue
+        if state != " ":
+            results.append("</span>")
+        if new_state == "+":
+            results.append("<span class='add'>")
+        if new_state == "-":
+            results.append("<span class='del'>")
+        results.append(html.escape(word))
+        state = new_state
+    if state != " ":
+        results.append("</span>")
+    obj = obj.copy()
+    obj['diff'] = results
+    return obj
+
+@bottle.route("/api/feedback")
+@bottle.view("feedback.view")
+def feedback():
+    new = [prettify(o) for o in DATA if o['status'] == "new"]
+    saved = [prettify(o) for o in DATA if o['status'] == "saved"]
+    return { 'new': new, 'saved': saved }
+
+@bottle.route("/api/status", method=["POST", "OPTIONS"])
+def status():
+    data = json.load(bottle.request.body)
+    DATA.set_status(data['id'], data['status'])
+
+@bottle.route('/<file>')
+def static(file):
+    return bottle.static_file(file, root=".")
+
+@bottle.route('/')
+def index():
+    return bottle.static_file("index.html", root=".")
+
+# For debugging
+
+class EnableCors(object):
+    name = 'enable_cors'
+    api = 2
+
+    def apply(self, fn, context):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            bottle.response.headers['Access-Control-Allow-Origin'] = '*'
+            bottle.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+            bottle.response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+            if bottle.request.method != 'OPTIONS':
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+
+        return _enable_cors
+
+app = bottle.app()
+app.install(EnableCors())
+
+if __name__ == "__main__":
+    bottle.run(port=8000, debug=True, reloader=True)
---
+++ www/feedback.js
@@ -0,0 +1,129 @@
+function markdown(elt, tools) {
+    if (tools) tools.remove();
+    var text = elt.textContent;
+    if (tools) elt.insertBefore(tools, elt.childNodes[0]);
+    return text;
+}
+
+function Element(name, properties, children) {
+    if (!children) { children = properties; properties = {} }
+    var elt = document.createElement(name);
+    for (var i in properties) {
+        if (properties.hasOwnProperty(i)) elt[i] = properties[i];
+    }
+    function recurse(child) {
+        if (!child) return;
+        else if (Array.isArray(child)) child.map(recurse);
+        else if (typeof child === "string") elt.appendChild(document.createTextNode(child));
+        else elt.appendChild(child);
+    }
+    recurse(children);
+    return elt
+}
+
+var LOCK = false;
+
+function make_tools(node) {
+    var a_typo = Element("a", { href: "#" }, "Typo" );
+    var a_comment = Element("a", { href: "#" }, "Comment" );
+    var form = Element("textarea", { placeholder: "Comment here" }, []);
+    form.style.display = "none";
+    var tools = Element("div", { className: "tools" }, [a_typo, a_comment, form]);
+
+    a_typo.addEventListener("click", function(e) {
+        LOCK = true;
+        tools.remove()
+        node.contentEditable = true;
+        node.focus()
+        var old_text = markdown(node);
+        var editing = true;
+        node.addEventListener("blur", function() {
+            var new_text = markdown(node, tools);
+            if (editing && new_text !== old_text) {
+                console.log("Submitting typo correction");
+                submit_typo(old_text, new_text);
+            }
+            editing = false;
+            LOCK = false;
+        });
+        e.preventDefault();
+    });
+
+    a_comment.addEventListener("click", function(e) {
+        form.style.display = "block";
+        form.focus()
+        LOCK = true;
+        var editing = true;
+        form.addEventListener("blur", function() {
+            var comment = form.value;
+            var text = markdown(node, tools);
+            if (editing && text) {
+                console.log("Submitting comment");
+                submit_comment(text, comment);
+            }
+            editing = false;
+            form.style.display = "none";
+            form.textContent = "";
+            LOCK = false;
+        });
+        e.preventDefault();
+    });
+
+    return tools;
+}
+
+function typo_mode() {
+    var elts = document.querySelectorAll("p, li, pre, .note");
+    for (var i = 0; i < elts.length; i++) {
+        (function(form) {
+            elts[i].addEventListener("mouseenter", function() {
+                if (!LOCK) this.insertBefore(form, this.childNodes[0]);
+            });
+            elts[i].addEventListener("mouseleave", function() {
+                if (!LOCK) form.remove();
+            });
+        })(make_tools(elts[i]));
+    }
+}
+
+function bad_request() {
+    if (this.status !== 200) {
+        console.error("Something went wrong with the XHR!");
+    }
+}
+
+function submit_typo(oldt, newt) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", bad_request);
+    xhr.open("POST", "http://127.0.0.1:8000/api/typo");
+    xhr.send(JSON.stringify({'old': oldt, 'new': newt, 'url': location.pathname}));
+}
+
+function submit_comment(text, comment) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", bad_request);
+    xhr.open("POST", "http://127.0.0.1:8000/api/comment");
+    xhr.send(JSON.stringify({'text': text, 'comment': comment, 'url': location.pathname}));
+}
+
+function feedback_mode() {
+    var elts = document.querySelectorAll("button");
+    for (var i = 0; i < elts.length; i++) {
+        elts[i].addEventListener('click', function() {
+            submit_status(parseInt(this.parentNode.dataset.id), this.className);
+            this.parentNode.remove();
+        });
+    }
+}
+
+function submit_status(id, status) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", bad_request);
+    xhr.open("POST", "http://127.0.0.1:8000/api/status");
+    xhr.send(JSON.stringify({'id': id, 'status': status}));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (document.body.id == "feedback") feedback_mode();
+    else typo_mode();
+})
