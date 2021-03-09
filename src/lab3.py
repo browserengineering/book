pavpanchekha/emberfox

Update ch3 to replace x, y with cx, cy

src/lab3.py
@@ -91,8 +91,8 @@ class Layout:
         self.tokens = tokens
         self.display_list = []
 
-        self.x = HSTEP
-        self.y = VSTEP
+        self.cx = HSTEP
+        self.cy = VSTEP
         self.weight = "normal"
         self.style = "roman"
         self.size = 16
@@ -124,7 +124,7 @@ class Layout:
             self.flush()
         elif tok.tag == "/p":
             self.flush()
-            self.y += VSTEP
+            self.cy += VSTEP
         
     def text(self, text):
         font = tkinter.font.Font(
@@ -134,23 +134,23 @@ class Layout:
         )
         for word in text.split():
             w = font.measure(word)
-            if self.x + w > WIDTH - HSTEP:
+            if self.cx + w > WIDTH - HSTEP:
                 self.flush()
-            self.line.append((self.x, word, font))
-            self.x += w + font.measure(" ")
+            self.line.append((self.cx, word, font))
+            self.cx += w + font.measure(" ")
 
     def flush(self):
         if not self.line: return
         metrics = [font.metrics() for x, word, font in self.line]
         max_ascent = max([metric["ascent"] for metric in metrics])
-        baseline = self.y + 1.2 * max_ascent
+        baseline = self.cy + 1.2 * max_ascent
         for x, word, font in self.line:
             y = baseline - font.metrics("ascent")
             self.display_list.append((x, y, word, font))
-        self.x = HSTEP
+        self.cx = HSTEP
         self.line = []
         max_descent = max([metric["descent"] for metric in metrics])
-        self.y = baseline + 1.2 * max_descent
+        self.cy = baseline + 1.2 * max_descent
 
 class Browser:
     def __init__(self):

browserengineering/book

src/lab3.py
@@ -90,8 +90,8 @@ class Layout:
         self.tokens = tokens
         self.display_list = []
 
-        self.x = HSTEP
-        self.y = VSTEP
+        self.row = VSTEP
+        self.col = HSTEP
         self.weight = "normal"
         self.style = "roman"
         self.size = 16
@@ -124,7 +124,7 @@ class Layout:
             self.flush()
         elif tok.tag == "/p":
             self.flush()
-            self.y += VSTEP
+            self.row += VSTEP
         
     def text(self, text):
         font = tkinter.font.Font(
@@ -134,23 +134,23 @@ class Layout:
         )
         for word in text.split():
             w = font.measure(word)
-            if self.x + w > WIDTH - HSTEP:
+            if self.col + w > WIDTH - HSTEP:
                 self.flush()
-            self.line.append((self.x, word, font))
-            self.x += w + font.measure(" ")
+            self.line.append((self.col, word, font))
+            self.col += w + font.measure(" ")
 
     def flush(self):
         if not self.line: return
         metrics = [font.metrics() for x, word, font in self.line]
         max_ascent = max([metric["ascent"] for metric in metrics])
-        baseline = self.y + 1.2 * max_ascent
+        baseline = self.row + 1.2 * max_ascent
         for x, word, font in self.line:
             y = baseline - font.metrics("ascent")
             self.display_list.append((x, y, word, font))
-        self.x = HSTEP
+        self.col = HSTEP
         self.line = []
         max_descent = max([metric["descent"] for metric in metrics])
-        self.y = baseline + 1.2 * max_descent
+        self.row = baseline + 1.2 * max_descent
 
 class Browser:
     def __init__(self):