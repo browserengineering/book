a88be714e198c68dac2b1d433afd25e798c84884
Chris Harrelson <chrishtr@gmail.com>
2021-04-29 22:24:58 -0700

    More edits

--- book/rendering-architecture.md
+++ book/rendering-architecture.md
@@ -299,10 +299,12 @@ var cur_frame_time = start_time
 
 function callback() {
     var output = document.querySelectorAll("#output")[0];
+    var since_last_frame = Date.now() - cur_frame_time;
+    var total_elapsed = Date.now() - start_time;
     output.innerHTML = "count: " + (count++) + "<br>" +
         " time elapsed since last frame: " + 
-        (Date.now() - cur_frame_time) + "ms" +
-        " total time elapsed: " + (Date.now() - start_time) + "ms";
+        since_last_frame + "ms" +
+        " total time elapsed: " + total_elapsed + "ms";
     if (count <= 100)
         requestAnimationFrame(callback);
     cur_frame_time = Date.now()
--- src/eventloop13.js
+++ src/eventloop13.js
@@ -4,10 +4,12 @@ var cur_frame_time = start_time
 
 function callback() {
 	var output = document.querySelectorAll("#output")[0];
-	var diff = Date.now() - cur_frame_time;
-	output.innerHTML = "count: " + (count++) + "<br>" +
-		" time elapsed since last frame: " +  (Date.now() - cur_frame_time) + "ms" +
-		" total time elapsed: " + (Date.now() - start_time) + "ms";
+  var since_last_frame = Date.now() - cur_frame_time;
+  var total_elapsed = Date.now() - start_time;
+  output.innerHTML = "count: " + (count++) + "<br>" +
+      " time elapsed since last frame: " + 
+      since_last_frame + "ms" +
+      " total time elapsed: " + total_elapsed + "ms";
 	if (count <= 100)
 		requestAnimationFrame(callback);
 	cur_frame_time = Date.now()
