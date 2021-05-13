aa1f76aa3c898f0157257d6c59f178da042e3d1b
Pavel Panchekha <me@pavpanchekha.com>
2021-05-13 17:45:47 -0600

    Move compare script to infra/

--- infra/compare.py
+++ infra/compare.py
@@ -103,10 +103,6 @@ def find_block(block, text):
 
     
 if __name__ == "__main__":
-    MIN_PYTHON = (3, 9)
-    if sys.version_info < MIN_PYTHON:
-        sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
-
     import sys, argparse
     argparser = argparse.ArgumentParser(description="Compare book blocks to teacher's copy")
     argparser.add_argument("book", metavar="book.md", type=argparse.FileType("r"))
