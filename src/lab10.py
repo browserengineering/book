bf74fad04c3cbbb46b42e9e3d5770e0ab3fced7b
Chris Harrelson <chrishtr@gmail.com>
2021-06-02 22:18:57 -0700

    Change comment

--- src/lab10.py
+++ src/lab10.py
@@ -1,6 +1,6 @@
 """
 This file compiles the code in Web Browser Engineering,
-up to and including Chapter 11 (Keeping Data Private),
+up to and including Chapter 10 (Keeping Data Private),
 without exercises.
 """
 
