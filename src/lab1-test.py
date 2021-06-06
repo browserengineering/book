cddb54dcb5071e7057c80da3f51bd7a8dcead8ac
Chris Harrelson <chrishtr@gmail.com>
2021-06-05 17:30:19 -0700

    none

--- src/lab1-test.py
+++ src/lab1-test.py
@@ -45,18 +45,18 @@ def mocked_socket(*args, **kwargs):
 	return MockSocket()
 
 class TestLab1(unittest.TestCase):
-  def test_show(self):
-    self.assertEqual('hello', lab1.get_text('hello'))
-    self.assertEqual('hello', lab1.get_text('<body>hello</body>'))
-    self.assertEqual('hello', lab1.get_text('he<body>llo</body>'))
-    self.assertEqual('hello', lab1.get_text('he<body>l</body>lo'))
-    self.assertEqual('hello', lab1.get_text('he<body>l<div>l</div>o</body>'))
+ 	def test_show(self):
+		self.assertEqual('hello', lab1.get_text('hello'))
+		self.assertEqual('hello', lab1.get_text('<body>hello</body>'))
+ 		self.assertEqual('hello', lab1.get_text('he<body>llo</body>'))
+ 		self.assertEqual('hello', lab1.get_text('he<body>l</body>lo'))
+ 		self.assertEqual('hello', lab1.get_text('he<body>l<div>l</div>o</body>'))
 
-  @mock.patch('socket.socket', side_effect=mocked_socket)
-  def test_request(self, mock_socket):
-  	(headers, body) = lab1.request('http://browser.engineering')
-  	self.assertEqual('Body text', body)
-  	self.assertEqual('Value1', headers['header1'])
+ 	@mock.patch('socket.socket', side_effect=mocked_socket)
+	def test_request(self, mock_socket):
+ 		(headers, body) = lab1.request('http://browser.engineering')
+ 		self.assertEqual('Body text', body)
+		self.assertEqual('Value1', headers['header1'])
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
