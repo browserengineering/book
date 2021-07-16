fc3eacf17e60f03b47deb1e6d9cdcbb88548ad37
Pavel Panchekha <me@pavpanchekha.com>
2021-07-16 17:16:03 -0600

    Woops

--- infra/filter.lua
+++ infra/filter.lua
@@ -1,18 +1,5 @@
 -- Pass 1: Load configuration data
 
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
-
 local config = nil
 local chapters = nil
 
