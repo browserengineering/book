fa22f261aeb82132a1a981e509abde781048b406
Chris Harrelson <chrishtr@gmail.com>
2021-08-09 15:00:01 -0700

    revert lab6 change

--- src/lab6.py
+++ src/lab6.py
@@ -309,6 +309,29 @@ class InlineLayout:
         return "InlineLayout(x={}, y={}, width={}, height={})".format(
             self.x, self.y, self.width, self.height)
 
+class DocumentLayout:
+    def __init__(self, node):
+        self.node = node
+        self.parent = None
+        self.previous = None
+        self.children = []
+
+    def layout(self):
+        child = BlockLayout(self.node, self, None)
+        self.children.append(child)
+
+        self.width = WIDTH - 2*HSTEP
+        self.x = HSTEP
+        self.y = VSTEP
+        child.layout()
+        self.height = child.height + 2*VSTEP
+
+    def paint(self, display_list):
+        self.children[0].paint(display_list)
+
+    def __repr__(self):
+        return "DocumentLayout()"
+
 class DrawText:
     def __init__(self, x1, y1, text, font, color):
         self.top = y1
