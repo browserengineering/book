528298f7edc74a5eb534a8ecf16b5405fc3a553e
Pavel Panchekha <me@pavpanchekha.com>
2021-09-15 11:41:22 -0600

    Fix how Text() tokens are printed

--- src/lab3.py
+++ src/lab3.py
@@ -15,7 +15,7 @@ class Text:
         self.text = text
 
     def __repr__(self):
-        return "Text('{}'')".format(self.text)
+        return "Text('{}')".format(self.text)
 
 class Tag:
     def __init__(self, tag):
