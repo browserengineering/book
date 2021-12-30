5afa6946c978411adde6f15e9eb12131263d1447
Chris Harrelson <chrishtr@gmail.com>
2021-12-30 11:34:48 -0800

    none

--- src/lab14.py
+++ src/lab14.py
@@ -1,6 +1,6 @@
 """
 This file compiles the code in Web Browser Engineering,
-up to and including Chapter 11 (Adding Visual Effects),
+up to and including Chapter 14 (Images and Iframes),
 without exercises.
 """
 
