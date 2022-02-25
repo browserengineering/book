5ea63ce11d61214480a556f7b88ad46bf06118b5
Chris Harrelson <chrishtr@gmail.com>
2022-02-24 22:35:39 -0800

    none

--- src/example13.js
+++ src/example13.js
@@ -1,10 +1,11 @@
 var end_opacity = 0;
-var num_animation_frames = 100;
+var num_animation_frames = 120;
+var frames_remaining = num_animation_frames;
 function animate() {
-    if (num_animation_frames == 0) return;
+    if (frames_remaining == 0) return;
     var div = document.querySelectorAll("div")[0];
-    div.style = "opacity:" + (num_animation_frames / 100);
-    num_animation_frames--;
+    div.style = "opacity:" + (frames_remaining / num_animation_frames);
+    frames_remaining--;
     requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
