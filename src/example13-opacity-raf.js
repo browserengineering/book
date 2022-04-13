9bc70d38202b70115765551a2d3591a4e8ada439
Chris Harrelson <chrishtr@gmail.com>
2022-04-12 22:06:50 -0700

    none

--- src/example13-opacity-raf.js
+++ src/example13-opacity-raf.js
@@ -1,23 +1,15 @@
-var start_value = 0.999;
 var end_value = 0.1;
-var num_animation_frames = 120;
-var frames_remaining = num_animation_frames;
+var frames_remaining = 120;
 var go_down = true;
 function animate() {
     var div = document.querySelectorAll("div")[0];
-    var percent_remaining = frames_remaining / num_animation_frames;
-    if (go_down) {
-        div.style = "opacity:" +
-            (percent_remaining * start_value +
-                (1 - percent_remaining) * end_value);
-    } else {
-        div.style = "opacity:" +
-            ((1-percent_remaining) * start_value +
-                percent_remaining * end_value);
-    }
-    frames_remaining--;
-    if (frames_remaining < 0) {
-        frames_remaining = num_animation_frames;
+    var percent_remaining = frames_remaining / 120;
+    if (!go_down) percent_remaining = 1 - percent_remaining;
+    div.style = "opacity:" +
+        (percent_remaining * 0.999 +
+            (1 - percent_remaining) * 0.1);
+    if (frames_remaining-- == 0) {
+        frames_remaining = 120;
         go_down = !go_down;
     }
     requestAnimationFrame(animate);
