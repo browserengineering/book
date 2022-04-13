5f457046bdbaa67a411f5cd46f1410db70caa86d
Chris Harrelson <chrishtr@gmail.com>
2022-04-12 22:09:49 -0700

    Shorten the javascript #451

--- src/example13-width-raf.js
+++ src/example13-width-raf.js
@@ -1,23 +1,13 @@
-var start_value = 400;
-var end_value = 100;
-var num_animation_frames = 120;
-var frames_remaining = num_animation_frames;
 var go_down = true;
 function animate() {
     var div = document.querySelectorAll("div")[0];
-    var percent_remaining = frames_remaining / num_animation_frames;
-    if (go_down) {
-        div.style = "background-color:lightblue;width:" +
-            (percent_remaining * start_value +
-            (1 - percent_remaining) * end_value) + "px";
-    } else {
-        div.style = "background-color:lightblue;width:" +
-            ((1 - percent_remaining) * start_value +
-             percent_remaining * end_value) + "px";
-    }
-    frames_remaining--;
-    if (frames_remaining < 0) {
-        frames_remaining = num_animation_frames;
+    var percent_remaining = frames_remaining / 120;
+    if (!go_down) percent_remaining = 1 - percent_remaining;
+    div.style = "background-color:lightblue;width:" +
+        (percent_remaining * 400 +
+        (1 - percent_remaining) * 100) + "px";
+    if (frames_remaining-- == 0) {
+        frames_remaining = 120;
         go_down = !go_down;
     }
     requestAnimationFrame(animate);
