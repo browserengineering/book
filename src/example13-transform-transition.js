59cfd10f9b1547069dba1b83d22c12d85e799168
Chris Harrelson <chrishtr@gmail.com>
2022-05-01 12:25:15 -0700

    none

--- src/example13-transform-transition.js
+++ src/example13-transform-transition.js
@@ -15,8 +15,8 @@ var div = document.querySelectorAll("div")[0];
 function go() {
 	go_up = !go_up;
 	if (go_up)
-		div.style = "background-color:lightblue;transform:translate(0px,0px)";
+		div.style = "background-color:lightblue;transform:translate(0px,0px);opacity:0.1";
 	else
-		div.style = "background-color:lightblue;transform:translate(50px,50px)";
+		div.style = "background-color:lightblue;transform:translate(50px,50px);opacity:0.999";
 	setTimeout(go, 16*120);
 }
