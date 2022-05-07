8d5cf196ca1ce99f8f1cf22ee2d10495f99554fa
Chris Harrelson <chrishtr@gmail.com>
2022-05-07 14:59:43 -0700

    none

--- src/lab13.py
+++ src/lab13.py
@@ -1714,8 +1714,6 @@ class Browser:
             self.raster_tab()
         if self.needs_draw:
             self.paint_draw_list()
-            for d in self.draw_list:
-                print_tree(d)
             self.draw()
         self.measure_composite_raster_and_draw.stop()
         self.needs_composite = False
