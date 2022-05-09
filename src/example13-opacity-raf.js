53e022258ba5736fc57813ff7e3ec31affd697c2
Pavel Panchekha <me@pavpanchekha.com>
2022-05-09 17:17:21 -0600

    Fix lints

--- src/example13-opacity-raf.js
+++ src/example13-opacity-raf.js
@@ -1,17 +1,12 @@
-var frames_remaining = 120;
-var go_down = true;
 var div = document.querySelectorAll("div")[0];
+var total_frames = 120;
+var current_frame = 0;
+var change_per_frame = 0.999 / total_frames;
 function animate() {
-    var percent_remaining = frames_remaining / 120;
-    if (!go_down) percent_remaining = 1 - percent_remaining;
-    div.style = "opacity:" +
-        (percent_remaining * 0.999 +
-            (1 - percent_remaining) * 0.1);
-    if (frames_remaining-- == 0) {
-        go_down = !go_down
-        frames_remaining = 120;
-    }
-    return true;
+    current_frame++;
+    var new_opacity = current_frame * change_per_frame;
+    div.style = "opacity:" + new_opacity;
+    return current_frame < total_frames;
 }
 
 function run_animation_frame() {
