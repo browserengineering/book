e658386622038ad4fbd0cd78b40ebe442772d5f6
Pavel Panchekha <me@pavpanchekha.com>
2022-05-10 17:17:11 -0600

    Add buttons for fading in and out

--- src/example13-opacity-raf.js
+++ src/example13-opacity-raf.js
@@ -1,16 +1,38 @@
 var div = document.querySelectorAll("div")[0];
 var total_frames = 120;
 var current_frame = 0;
-var change_per_frame = 0.999 / total_frames;
-function animate() {
+var change_per_frame = 0.899 / total_frames;
+
+function fade_in() {
     current_frame++;
-    var new_opacity = current_frame * change_per_frame;
+    var new_opacity = current_frame * change_per_frame + 0.1;
     div.style = "opacity:" + new_opacity;
     return current_frame < total_frames;
 }
 
-function run_animation_frame() {
-    if (animate())
-        requestAnimationFrame(run_animation_frame);
+function run_fade_in() {
+    if (fade_in())
+        requestAnimationFrame(run_fade_in);
 }
-requestAnimationFrame(run_animation_frame);
+
+function start_fade_in() {
+    current_frame = 0;
+    requestAnimationFrame(run_fade_in);
+}
+
+function fade_out() {
+    current_frame++;
+    var new_opacity = 0.999 - current_frame * change_per_frame;
+    div.style = "opacity:" + new_opacity;
+    return current_frame < total_frames;
+}
+
+function run_fade_out() {
+    if (fade_out())
+        requestAnimationFrame(run_fade_out);
+}
+
+function start_fade_out() {
+    current_frame = 0;
+    requestAnimationFrame(run_fade_out);
+}
