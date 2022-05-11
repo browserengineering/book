0bbd2db289101ccb0a254db331e0247e255763fe
Pavel Panchekha <me@pavpanchekha.com>
2022-05-11 11:53:36 -0600

    Merged and tightened the first few sections (#504)
    * Merged and tightened the first few sections
    
    * Remind people to refresh
    
    * Fix lints
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Harrelson <3453258+chrishtr@users.noreply.github.com>
    
    * Woops, removed
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Harrelson <3453258+chrishtr@users.noreply.github.com>
    
    * Add buttons for fading in and out
    
    * Make it work in our browser
    
    * Woops!
    
    * Clarify where 0.899 comes from
    
    Co-authored-by: Chris Harrelson <3453258+chrishtr@users.noreply.github.com>

--- src/example13-opacity-raf.js
+++ src/example13-opacity-raf.js
@@ -1,21 +1,43 @@
-var frames_remaining = 120;
-var go_down = true;
 var div = document.querySelectorAll("div")[0];
-function animate() {
-    var percent_remaining = frames_remaining / 120;
-    if (!go_down) percent_remaining = 1 - percent_remaining;
-    div.style = "opacity:" +
-        (percent_remaining * 0.999 +
-            (1 - percent_remaining) * 0.1);
-    if (frames_remaining-- == 0) {
-        go_down = !go_down
-        frames_remaining = 120;
-    }
-    return true;
+var total_frames = 120;
+var current_frame = 0;
+var change_per_frame = (0.999 - 0.1) / total_frames;
+
+function fade_in() {
+    current_frame++;
+    var new_opacity = current_frame * change_per_frame + 0.1;
+    div.style = "opacity:" + new_opacity;
+    return current_frame < total_frames;
 }
 
-function run_animation_frame() {
-    if (animate())
-        requestAnimationFrame(run_animation_frame);
+function run_fade_in() {
+    if (fade_in())
+        requestAnimationFrame(run_fade_in);
 }
-requestAnimationFrame(run_animation_frame);
+
+function start_fade_in(e) {
+    current_frame = 0;
+    requestAnimationFrame(run_fade_in);
+    e.preventDefault();
+}
+
+function fade_out() {
+    current_frame++;
+    var new_opacity = 0.999 - current_frame * change_per_frame;
+    div.style = "opacity:" + new_opacity;
+    return current_frame < total_frames;
+}
+
+function run_fade_out() {
+    if (fade_out())
+        requestAnimationFrame(run_fade_out);
+}
+
+function start_fade_out(e) {
+    current_frame = 0;
+    requestAnimationFrame(run_fade_out);
+    e.preventDefault();
+}
+
+document.querySelectorAll("button")[0].addEventListener("click", start_fade_out);
+document.querySelectorAll("button")[1].addEventListener("click", start_fade_in);
