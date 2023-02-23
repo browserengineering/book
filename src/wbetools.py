179398bde88b3af73160e496ad48f40f9e506ffc
Pavel Panchekha <me@pavpanchekha.com>
2023-02-23 11:49:20 -0700

    Try to add support for patching functions (scary!)

--- src/wbetools.py
+++ src/wbetools.py
@@ -6,8 +6,11 @@ def patch(existing_cls):
     def decorator(new_cls):
         for attr in dir(new_cls):
             obj = getattr(new_cls, attr)
-            # This is a cute hack to get the `function` class
-            if isinstance(obj, type(decorator)):
+            # Copies over all methods and all fields of a function object
+            # Uses a cute hack to get the `function` class.
+            if isinstance(obj, type(decorator)) \
+               or attr in ["__code__", "__globals__", "__module__",
+                           "__defaults__", "__closure__", "__annotations"]:
                 setattr(existing_cls, attr, obj)
         return existing_cls
     return decorator
