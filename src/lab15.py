5ba7e263865bd11eeb200d9ba0ed9347f1d06309
Chris Harrelson <3453258+chrishtr@users.noreply.github.com>
2023-02-24 20:04:26 -0800

    Add broken image support (#738)

--- src/lab15.py
+++ src/lab15.py
@@ -739,8 +739,12 @@ def download_image(image_src, frame):
     image_url = resolve_url(image_src, frame.url)
     assert frame.allowed_request(image_url), \
         "Blocked load of " + image_url + " due to CSP"
-    header, body = request(image_url, frame.url)
-    data = skia.Data.MakeWithoutCopy(body)
+    try:
+        header, body = request(image_url, frame.url)
+        data = skia.Data.MakeWithoutCopy(body)
+    except:
+        data = skia.Data.MakeFromFileName("Broken_Image.png")
+        body = ""
     img = skia.Image.MakeFromEncoded(data)
     assert img, "Failed to recognize image format for " + image_url
     return body, img
