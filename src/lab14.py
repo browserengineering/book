d5a3085d0625689a6316c66bcb976fb00d492346
Chris Harrelson <3453258+chrishtr@users.noreply.github.com>
2023-06-05 10:56:23 -0700

    Add set_needs_render to setAttribute, since it affects roles and layout (#838)
    * Add set_needs_render to setAttribute, since it affects roles and layout
    
    * none

--- src/lab14.py
+++ src/lab14.py
@@ -856,6 +856,7 @@ class JSContext:
     def setAttribute(self, handle, attr, value):
         elt = self.handle_to_node[handle]
         elt.attributes[attr] = value
+        self.tab.set_needs_render()
         
     def innerHTML_set(self, handle, s):
         doc = HTMLParser(
@@ -1284,7 +1285,7 @@ class Browser:
                 self.skia_context,
                 skia.GrBackendRenderTarget(
                     WIDTH, HEIGHT, 0, 0, 
-                    skia.GrGLFramebufferInfo(0, GL.GL_RGBA8)),
+                    skia.GrGLFramebufferInfo(0, OpenGL.GL.GL_RGBA8)),
                     skia.kBottomLeft_GrSurfaceOrigin,
                     skia.kRGBA_8888_ColorType,
                     skia.ColorSpace.MakeSRGB())
