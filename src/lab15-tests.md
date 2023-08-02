a764ca2beca04a1a2600d2440be4909ba3237608
Pavel Panchekha <me@pavpanchekha.com>
2023-08-01 23:09:20 -0600

    Minor repo organization work  (#881)
    * Delete old reflow chapter code (obsolete)
    
    * Unify mock socket
    
    * Merge test and test11

--- src/lab15-tests.md
+++ src/lab15-tests.md
@@ -5,7 +5,7 @@ This file contains tests for Chapter 15 (Embedded Content).
 
     >>> from test import Event
     >>> import threading
-    >>> import test11 as test
+    >>> import test
     >>> _ = test.socket.patch().start()
     >>> _ = test.ssl.patch().start()
     >>> _ = test.gtts.patch()
--- src/test.py
+++ src/test.py
@@ -9,6 +9,9 @@ import sys
 import tkinter
 import tkinter.font
 import unittest
+import sdl2
+import skia
+import threading
 from unittest import mock
 
 class socket:
@@ -18,19 +21,11 @@ class socket:
     def __init__(self, *args, **kwargs):
         self.request = b""
         self.connected = False
-        self.scheme = "http"
-        self.ssl_hostname = None
 
     def connect(self, host_port):
+        self.scheme = "http"
         self.host, self.port = host_port
         self.connected = True
-        self._check_cert()
-
-    def _check_cert(self):
-        if self.connected and self.host and self.ssl_hostname:
-            assert self.host == self.ssl_hostname, "server_hostname does not match the host"
-            if self.host.endswith(".badssl.com"): # Fake badssl.com
-                raise ssl.SSLCertVerificationError()
 
     def send(self, text):
         self.request += text
@@ -43,9 +38,8 @@ class socket:
             assert all(int(value) == len(self.body) for name, value in headers
                        if name.lower() == "content-length")
 
-    def makefile(self, mode, encoding, newline):
-        assert self.connected and self.host and self.port, \
-            "You cannot call makefile() on a socket until you call connect() and send()"
+    def makefile(self, mode, encoding=None, newline=None):
+        assert self.connected and self.host and self.port
         if self.port == 80 and self.scheme == "http":
             url = self.scheme + "://" + self.host + self.path
         elif self.port == 443 and self.scheme == "https":
@@ -57,7 +51,14 @@ class socket:
         output = self.URLs[url][1]
         if self.URLs[url][2]:
             assert self.body == self.URLs[url][2], (self.body, self.URLs[url][2])
-        return io.StringIO(output.decode(encoding).replace(newline, "\n"), newline)
+        stream = io.BytesIO(output)
+        if encoding:
+            stream = io.TextIOWrapper(stream, encoding=encoding, newline=newline)
+            stream.mode = mode
+        else:
+            assert mode == "b", "If no file encoding is passed, must pass 'b' mode"
+
+        return stream
 
     def close(self):
         self.connected = False
@@ -73,7 +74,7 @@ class socket:
     @classmethod
     def respond_ok(cls, url, response, method="GET", body=None):
         response = ("HTTP/1.0 200 OK\r\n\r\n" + response).encode("utf8")
-        cls.respond(url, response, method=method, body=body)
+        cls.URLs[url] = [method, response, body]
 
     @classmethod
     def serve(cls, html):
@@ -101,8 +102,7 @@ class socket:
 
 class ssl:
     def wrap_socket(self, s, server_hostname):
-        s.ssl_hostname = server_hostname
-        s._check_cert()
+        assert s.host == server_hostname
         s.scheme = "https"
         return s
 
@@ -143,7 +143,7 @@ class SilentCanvas:
 
 tkinter.Canvas = SilentCanvas
 
-class MockCanvas:
+class TkCanvas:
     def __init__(self, *args, **kwargs):
         pass
 
@@ -164,12 +164,12 @@ class MockCanvas:
 original_tkinter_canvas = tkinter.Canvas
 
 def patch_canvas():
-    tkinter.Canvas = MockCanvas
+    tkinter.Canvas = TkCanvas
 
 def unpatch_canvas():
     tkinter.Canvas = original_tkinter_canvas
 
-class MockFont:
+class TkFont:
     def __init__(self, size=None, weight=None, slant=None, style=None):
         self.size = size
         self.weight = weight
@@ -190,7 +190,7 @@ class MockFont:
         return "Font size={} weight={} slant={} style={}".format(
             self.size, self.weight, self.slant, self.style)
 
-tkinter.font.Font = MockFont
+tkinter.font.Font = TkFont
 
 def breakpoint(name, *args):
     args_str = (", " + ", ".join(["'{}'".format(arg) for arg in args]) if args else "")
@@ -211,3 +211,246 @@ class Event:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+class gtts:
+    class gTTS:
+        def __init__(self, text):
+            pass
+        def save(self, file):
+            pass
+
+    @classmethod
+    def patch(cls):
+        import sys
+        sys.modules["gtts"] = cls()
+
+
+
+def SDL_GetWindowSurfacePatched(window):
+    return None
+
+sdl2.SDL_GetWindowSurface = SDL_GetWindowSurfacePatched
+
+def SDL_BlitSurfacePatched(surface, rect, window_surface, rect2):
+    return None
+
+sdl2.SDL_BlitSurface = SDL_BlitSurfacePatched
+
+
+def SDL_UpdateWindowSurfacePatched(window):
+    return None
+
+sdl2.SDL_UpdateWindowSurface = SDL_UpdateWindowSurfacePatched
+
+class MockSkiaImage:
+    def __init__(self):
+        pass
+
+    def tobytes(self):
+        return ""
+
+class MockFont:
+    def __init__(self, typeface, size):
+        self.size = size
+        self.typeface = typeface
+
+    def measureText(self, word):
+        return self.size * len(word)
+
+    def getMetrics(self, name=None):
+        m = skia.FontMetrics()
+        m.fAscent = -self.size * .75
+        m.fDescent = self.size * .25
+        return m
+
+    def __repr__(self):
+        return "Font size={} weight={} slant={} style={}".format(
+            self.size, self.weight, self.slant, self.style)
+
+class MockCanvas:
+    def __init__(self):
+        self.commands = []
+
+    def clear(self, color):
+        self.commands.append("clear(color={:x})".format(color))
+
+    def format_paint(paint, include_leading_comma=True):
+        format_str = ""
+        if include_leading_comma:
+            format_str = ", "
+        format_str = format_str + "color={color:x}"
+        if paint.getAlpha() != 255:
+            format_str = format_str + ", alpha={alpha}"
+        if paint.getBlendMode() != skia.BlendMode.kSrcOver:
+            format_str = format_str + ", blend_mode={blend_mode}"
+        return format_str
+
+    def drawRect(self, rect, paint):
+        format_str = "drawRect(rect={rect}" + MockCanvas.format_paint(paint)
+        self.commands.append(
+            (format_str + ")").format(
+            rect=rect, color=paint.getColor(),
+            alpha=paint.getAlpha(), blend_mode=paint.getBlendMode()))
+
+    def drawPath(self, path, paint):
+        format_str = "drawPath(<path>" + MockCanvas.format_paint(paint)
+        self.commands.append(
+            (format_str + ")").format(
+            color=paint.getColor(),
+            alpha=paint.getAlpha(), blend_mode=paint.getBlendMode()))
+
+    def drawCircle(self, cx, cy, radius, paint):
+        format_str = "drawCircle(cx={cx}, cy={cy}, radius={radius}" \
+            + MockCanvas.format_paint(paint)
+        self.commands.append(
+            (format_str + ")").format(
+                cx=cx, cy=cy, radius=radius,
+                color=paint.getColor(),
+                alpha=paint.getAlpha(), blend_mode=paint.getBlendMode()))
+
+    def drawString(self, text, x, y, font, paint):
+        format_str = "drawString(text={text}, x={x}, y={y}" \
+            + MockCanvas.format_paint(paint)
+        self.commands.append((format_str + ")").format(
+            text=text, x=x, y=y,
+            color=paint.getColor(),
+            alpha=paint.getAlpha(), blend_mode=paint.getBlendMode()))
+
+    def save(self):
+        self.commands.append("save()")
+
+    def saveLayer(self, paint):
+        format_str = "saveLayer(" + MockCanvas.format_paint(paint, False)
+        self.commands.append((format_str + ")").format(
+            color=paint.getColor(),
+            alpha=paint.getAlpha(), blend_mode=paint.getBlendMode()))
+
+    def clipRect(self, rect):
+        self.commands.append("clipRect(rect={rect})".format(rect=rect))
+
+    def clipRRect(self, rrect):
+        self.commands.append(
+            "clipRRect(bounds={bounds}, radius={radius})".format(
+                bounds=rrect.getBounds(), radius=rrect.getSimpleRadii()))
+
+    def drawRRect(self, rrect, paint):
+       format_str = "drawRRect(bounds={bounds}, radius={radius}, " + \
+           MockCanvas.format_paint(paint, False)
+       self.commands.append(
+           (format_str + ")").format(
+           bounds=rrect.getBounds(), radius=rrect.getSimpleRadii(),
+           color=paint.getColor(),
+          alpha=paint.getAlpha(), blend_mode=paint.getBlendMode()))
+
+
+    def drawImage(self, image, left, top):
+        self.commands.append("drawImage(<image>, left={left}, top={top}".format(
+            left=left, top=top))
+
+    def drawImageRect(self, image, src, dst):
+        self.commands.append(
+            "drawImageRect(<image>, src={src}, dst={dst}".format(
+                src=src, dst=dst))
+
+    def restore(self):
+        self.commands.append("restore()")
+
+    def translate(self, x, y):
+        self.commands.append("translate(x={x}, y={y})".format(
+            x=x, y=y))
+
+    def rotate(self, degrees):
+        self.commands.append("rotate(degrees={degrees})".format(
+            degrees=degrees))
+
+class MockSkiaSurface:
+    def __init__(self, width, height):
+        self.canvas = MockCanvas()
+        pass
+
+    @classmethod
+    def MakeRaster(cls, info):
+        return cls(info.width, info.height)
+
+    def getCanvas(self):
+        return self.canvas
+
+    def makeImageSnapshot(self):
+        return MockSkiaImage()
+
+    def printTabCommands(self):
+        count = 0
+        total = len(self.canvas.commands)
+        for command in self.canvas.commands:
+            print(command)
+            count = count + 1
+
+    def draw(self, canvas, x, y):
+        pass
+
+skia.Surface = MockSkiaSurface
+skia.Font = MockFont
+
+class MockTimer:
+	def __init__(self, sec, callback):
+		self.sec = sec
+		self.callback = callback
+
+	def start(self):
+		self.callback()
+
+	def cancel(self):
+		self.callback = None
+
+threading.Timer = MockTimer
+
+class MockTaskRunner:
+	def __init__(self, tab):
+		self.tab = tab
+
+	def schedule_task(self, callback):
+		callback()
+
+	def clear_pending_tasks(self):
+		pass
+
+	def start(self):
+		pass
+
+	def run(self):
+		pass
+
+class MockNoOpTaskRunner:
+	def __init__(self, tab):
+		self.tab = tab
+
+	def schedule_task(self, callback):
+		pass
+
+	def start(self):
+		pass
+
+	def run(self):
+		pass
+
+class MockLock:
+	def acquire(self, blocking): pass
+	def release(self): pass
+
+	@classmethod
+	def patch(cls):
+		return mock.patch("threading.Lock", wraps=cls)
+
+
+def print_display_list_skip_noops(display_list):
+		for item in display_list:
+				print_tree_skip_nooops(item)
+
+def print_tree_skip_nooops(node, indent=0):
+    if node.__repr__().find("no-op") >= 0:
+        extra = 0
+    else:
+        print(" " * indent, node)
+        extra = 2
+    for child in node.children:
+        print_tree_skip_nooops(child, indent + extra)
