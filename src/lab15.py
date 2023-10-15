7a58ec2e413f831cd856745dbb16480eb12d8848
Chris Harrelson <chrishtr@gmail.com>
2023-10-15 15:37:09 -0700

    none

--- src/lab15.py
+++ src/lab15.py
@@ -133,7 +133,7 @@ def paint_tree(layout_object, display_list):
         layout_object.node.frame:
         paint_tree(layout_object.node.frame.document, cmds)
     else:
-        for child in layout_object.children.get():
+        for child in layout_object.children:
             paint_tree(child, cmds)
 
     cmds = layout_object.paint_effects(cmds)
