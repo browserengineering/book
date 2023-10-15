7ae6c3859aae014b7f518fe627dd1ccc3cdda0fc
Chris Harrelson <chrishtr@gmail.com>
2023-10-15 16:12:08 -0700

    none

--- src/lab4.py
+++ src/lab4.py
@@ -38,9 +38,8 @@ class Element:
 
 def print_tree(node, indent=0):
     print(" " * indent, node)
-    if hasattr(node, "children"):
-        for child in node.children:
-            print_tree(child, indent + 2)
+    for child in node.children:
+        print_tree(child, indent + 2)
 
 class HTMLParser:
     def __init__(self, body):
