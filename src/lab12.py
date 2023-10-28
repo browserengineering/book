fbe91dd202e1712c2269ec43a138f438c9e410df
Chris Harrelson <chrishtr@gmail.com>
2023-10-28 15:14:56 -0700

    none

--- src/lab12.py
+++ src/lab12.py
@@ -22,6 +22,7 @@ from lab5 import BLOCK_ELEMENTS, DocumentLayout
 from lab6 import CSSParser, TagSelector, DescendantSelector
 from lab6 import INHERITED_PROPERTIES, style, cascade_priority
 from lab6 import tree_to_list
+from lab7 import Rect
 from lab8 import Text, Element, INPUT_WIDTH_PX
 from lab9 import EVENT_DISPATCH_JS
 from lab10 import COOKIE_JAR, JSContext, URL
@@ -427,7 +428,7 @@ class Chrome:
             self.address_bar = ""
         else:
             for i, tab in enumerate(self.browser.tabs):
-                if self.tab_rect(i).contains(x, y, self.tab_rect):
+                if self.tab_rect(i).contains(x, y):
                     self.browser.set_active_tab(tab)
                     active_tab = self.browser.active_tab
                     task = Task(active_tab.set_needs_render)
