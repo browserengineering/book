e913299b21aac8a99435e5da0ccedbd3f8520902
Pavel Panchekha <me@pavpanchekha.com>
2023-11-15 13:38:41 -0700

    Add RGBA color support

--- src/lab11.py
+++ src/lab11.py
@@ -65,6 +65,12 @@ def parse_color(color):
         g = int(color[3:5], 16)
         b = int(color[5:7], 16)
         return skia.Color(r, g, b)
+    elif color.startswith("#") and len(color) == 9:
+        r = int(color[1:3], 16)
+        g = int(color[3:5], 16)
+        b = int(color[5:7], 16)
+        a = int(color[7:9], 16)
+        return skia.Color(r, g, b, a)
     elif color in NAMED_COLORS:
         return parse_color(NAMED_COLORS[color])
     else:
