feab8152817bbbc40bba07cf0fc63e7f16812460
Chris Harrelson <3453258+chrishtr@users.noreply.github.com>
2023-12-30 21:47:11 -0800

    Don't crash on malformed URLs (#1106)
    * Don't crash on malformed URLs
    
    * none

--- src/lab1.py
+++ src/lab1.py
@@ -10,22 +10,27 @@ import wbetools
 
 class URL:
     def __init__(self, url):
-        self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https"]
+        try:
+            self.scheme, url = url.split("://", 1)
+            assert self.scheme in ["http", "https"]
 
-        if "/" not in url:
-            url = url + "/"
-        self.host, url = url.split("/", 1)
-        self.path = "/" + url
+            if "/" not in url:
+                url = url + "/"
+            self.host, url = url.split("/", 1)
+            self.path = "/" + url
 
-        if self.scheme == "http":
-            self.port = 80
-        elif self.scheme == "https":
-            self.port = 443
+            if self.scheme == "http":
+                self.port = 80
+            elif self.scheme == "https":
+                self.port = 443
 
-        if ":" in self.host:
-            self.host, port = self.host.split(":", 1)
-            self.port = int(port)
+            if ":" in self.host:
+                self.host, port = self.host.split(":", 1)
+                self.port = int(port)
+        except:
+            print("Malformed URL found, falling back to the WBE home page.")
+            print("  URL was: " + url)
+            self.__init__("https://browser.engineering")
 
     def request(self):
         s = socket.socket(
