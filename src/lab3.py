b87b8570e381eaf0589c28576c701572b7fc8545
Shinya Fujino <shf0811@gmail.com>
2024-04-04 00:48:51 +0900

    Remove `__repr__` from lab3.py

--- src/lab3.py
+++ src/lab3.py
@@ -16,15 +16,9 @@ class Text:
     def __init__(self, text):
         self.text = text
 
-    def __repr__(self):
-        return "Text('{}')".format(self.text)
-
 class Tag:
     def __init__(self, tag):
         self.tag = tag
-
-    def __repr__(self):
-        return "Tag('{}')".format(self.tag)
 
 def lex(body):
     out = []
