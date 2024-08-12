f9ce430e84a348a7ddc6c77e402f9dccd36197ed
Pavel Panchekha <me@pavpanchekha.com>
2024-08-12 16:29:56 -0600

    Text chapter cahnges

--- src/lab3.py
+++ src/lab3.py
@@ -52,8 +52,7 @@ FONTS = {}
 def get_font(size, weight, style):
     key = (size, weight, style)
     if key not in FONTS:
-        font = tkinter.font.Font(size=size, weight=weight,
-            slant=style)
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
         label = tkinter.Label(font=font)
         FONTS[key] = (font, label)
     return FONTS[key][0]
