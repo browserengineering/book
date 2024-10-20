b8c0d6f313748094b5f29d0252df9597d29429a8
Ryuan Choi <ryuan.choi@navercorp.com>
2024-10-20 09:51:48 +0900

    Ch 11: follow the Rect class introduced in #974
    In #974, the Rect class is introduced in Chapter 7 (chrome.md),
    but it seems that visual-effects.md hasn't been updated accordingly.

--- src/lab13.py
+++ src/lab13.py
@@ -119,16 +119,12 @@ class Transform(VisualEffect):
 class DrawLine(PaintCommand):
     def __init__(self, x1, y1, x2, y2, color, thickness):
         super().__init__(skia.Rect.MakeLTRB(x1, y1, x2, y2))
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
         self.color = color
         self.thickness = thickness
 
     def execute(self, canvas):
-        path = skia.Path().moveTo(self.x1, self.y1) \
-                          .lineTo(self.x2, self.y2)
+        path = skia.Path().moveTo(self.rect.left(), self.rect.top()) \
+                          .lineTo(self.rect.right(), self.rect.bottom())
         paint = skia.Paint(
             Color=parse_color(self.color),
             StrokeWidth=self.thickness,
@@ -139,7 +135,8 @@ class DrawLine(PaintCommand):
     @wbetools.js_hide
     def __repr__(self):
         return "DrawLine top={} left={} bottom={} right={}".format(
-            self.y1, self.x1, self.y2, self.x2)
+            self.rect.top(), self.rect.left(),
+            self.rect.bottom(), self.rect.right())
 
 class DrawRRect(PaintCommand):
     def __init__(self, rect, radius, color):
@@ -160,23 +157,21 @@ class DrawRRect(PaintCommand):
 
 class DrawText(PaintCommand):
     def __init__(self, x1, y1, text, font, color):
-        self.left = x1
-        self.top = y1
-        self.right = x1 + font.measureText(text)
-        self.bottom = y1 - font.getMetrics().fAscent + font.getMetrics().fDescent
         self.font = font
         self.text = text
         self.color = color
-        super().__init__(skia.Rect.MakeLTRB(x1, y1,
-            self.right, self.bottom))
+        super().__init__(skia.Rect.MakeLTRB(
+            x1, y1,
+            x1 + font.measureText(text),
+            y1 - font.getMetrics().fAscent + font.getMetrics().fDescent))
 
     def execute(self, canvas):
         paint = skia.Paint(
             AntiAlias=True,
             Color=parse_color(self.color)
         )
-        baseline = self.top - self.font.getMetrics().fAscent
-        canvas.drawString(self.text, float(self.left), baseline,
+        baseline = self.rect.top() - self.font.getMetrics().fAscent
+        canvas.drawString(self.text, float(self.rect.left()), baseline,
             self.font, paint)
 
     @wbetools.js_hide
